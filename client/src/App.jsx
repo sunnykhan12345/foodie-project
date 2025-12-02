24aeaa02e3c5721f6e526f64e377e22b0d3e1626
sana ullah <sunnykhansadda@gmail.com>
2025-12-03 00:16:38 +0500

    working on button setions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,95 +1,14 @@
-
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div className="max-padd-container">
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
+      <button className="btn-solid mt-3">Submit</button>
+      <button className="btn-light m-3">Submit</button>
+      <button className="btn-outline m-3">Submit</button>
+      <button className="btn-white m-3">Submit</button>
     </div>
   );
-}
+};
 
-export default App
+export default App;
