7bb11a0e359ae1941d5eca198d8700ac3b636d1a
sana ullah <sunnykhansadda@gmail.com>
2025-12-02 22:44:41 +0500

    meanrstuck food projects

---
+++ client/src/App.jsx
@@ -0,0 +1,95 @@
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="max-padd-container">
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+    </div>
+  );
+}
+
+export default App
