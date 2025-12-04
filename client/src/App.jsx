2bbf6752ac91fa0a56b6f2ce9d6a5023b28f7803
sana ullah <sunnykhansadda@gmail.com>
2025-12-04 16:07:32 +0500

    working on routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,30 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Blog from "./pages/Blog";
+import Conatct from "./pages/Conatct";
+import Cart from "./pages/Cart";
+import AddressForm from "./pages/AddressForm";
+import MyOrder from "./pages/MyOrder";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 const App = () => {
   return (
-    <div className="max-padd-container">
-      <button className="btn-solid mt-3">Submit</button>
-      <button className="btn-light m-3">Submit</button>
-      <button className="btn-outline m-3">Submit</button>
-      <button className="btn-white m-3">Submit</button>
-    </div>
+    <main className="max-padd-container overflow-hidden text-textColor">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Conatct />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/address-form" element={<AddressForm />} />
+        <Route path="/my-order" element={<MyOrder />} />
+      </Routes>
+      <Footer />
+    </main>
   );
 };
 
---
+++ client/src/pages/AddressForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddressForm = () => {
+  return (
+    <div>
+      addedss form
+    </div>
+  )
+}
+
+export default AddressForm
